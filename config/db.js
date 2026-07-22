8e9b9d6f5d69661f65c2e835e7bb31271425aa17
aryan000singh <aryan03aryansingh@bbdnitm.ac.in>
2026-07-22 23:57:11 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
-import dns from "dns";
 
-//
-dns.setServers(["8.8.8.8", "1.1.1.1"])
 let isConnected = false;
 
 const connectDB = async () => {
   if (isConnected) return;
- 
-  await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
-  console.log("MongoDB Connected");
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    throw error;
+  }
 };
 
 export default connectDB;
